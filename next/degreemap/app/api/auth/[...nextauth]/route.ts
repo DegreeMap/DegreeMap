1a9f82546567656de4d0ef1d364cf84efb7d565e
Christian <cmmorgado53@gmail.com>
2024-06-05 12:42:38 -0400

    trying out google

--- next/degreemap/app/api/auth/[...nextauth]/route.ts
+++ next/degreemap/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,14 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
--- next/degreemap/components/auth/buttons.tsx
+++ next/degreemap/components/auth/buttons.tsx
@@ -1,9 +1,11 @@
 "use client"
 
+// import { getServerSession } from "next-auth";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function AuthButton() {
     const {data: session} = useSession();
+    // const session = await getServerSession(); // also make the function async
     
     if(session){
         return (
@@ -14,6 +16,12 @@ export default function AuthButton() {
                 <br></br>
                 <p>signed in!</p>
                 <button onClick={() => signOut()}>click here to sign out</button>
+                <br></br>
+                <br></br>
+                <p>server session result:</p>
+                <p> {`name: ${session?.user?.name}`}</p>
+                <p> {`email: ${session?.user?.email}`}</p>
+                <p> {`image: ${session?.user?.image}`}</p>
             </>
         );
     }
