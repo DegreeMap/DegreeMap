a3f9e3a1570eb3d00012ca94d5c8ace83c682917
Christian <cmmorgado53@gmail.com>
2024-06-20 23:04:01 -0400

    switched strat to identity

--- server/src/main/java/com/degreemap/DegreeMap/tags/Tag.java
+++ server/src/main/java/com/degreemap/DegreeMap/tags/Tag.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 public class Tag {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
