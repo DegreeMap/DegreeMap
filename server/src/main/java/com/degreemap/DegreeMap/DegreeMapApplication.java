a8c4faae173c04d392fbaa4d858cc094035d7316
Raynard Miot <rayhmiot@gmail.com>
2024-06-22 17:45:05 +0800

    Hid away secrets, working on shifting from HMAC to RSA encoding/decoding for JWTs

--- server/src/main/java/com/degreemap/DegreeMap/DegreeMapApplication.java
+++ server/src/main/java/com/degreemap/DegreeMap/DegreeMapApplication.java
@@ -1,9 +1,12 @@
 package com.degreemap.DegreeMap;
 
+import com.degreemap.DegreeMap.config.RSAKeyRecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RSAKeyRecord.class)
 public class DegreeMapApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DegreeMapApplication.class, args);
---
+++ server/src/main/java/com/degreemap/DegreeMap/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.degreemap.DegreeMap.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
