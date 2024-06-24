12012b7e4f2a6c99500f823a46fd1ea6f5bf8f43
Raynard Miot <rayhmiot@gmail.com>
2024-06-24 23:57:19 +0800

    Added 'User' role to `SecurityUser`

--- server/src/main/java/com/degreemap/DegreeMap/auth/SecurityUser.java
+++ server/src/main/java/com/degreemap/DegreeMap/auth/SecurityUser.java
@@ -2,6 +2,7 @@ package com.degreemap.DegreeMap.auth;
 
 import com.degreemap.DegreeMap.users.User;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -17,7 +18,7 @@ public class SecurityUser implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(new SimpleGrantedAuthority("USER"));
     }
 
     @Override
