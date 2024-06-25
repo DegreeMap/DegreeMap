66fec680cd2627e72447de051a34c71d2edbd71b
Raynard Miot <rayhmiot@gmail.com>
2024-06-25 11:01:00 +0800

    Decoupled logic for authenticating user and logic for returning JWT

--- server/src/main/java/com/degreemap/DegreeMap/auth/AuthService.java
+++ server/src/main/java/com/degreemap/DegreeMap/auth/AuthService.java
@@ -22,9 +22,22 @@ public class AuthService {
         this.jwtGenerator = jwtGenerator;
     }
 
-
-    // TODO: Decouple logic for authenticating user and logic for returning JWT
-    public AuthResponseDto getAccessTokenFromCredentials(String email, String password) {
+    /**
+     * Attempts to authenticate a user based on a given email and password.
+     * @param email email to authenticate with
+     * @param password password to authenticate with
+     * @return the UserDetails of that user.
+     * @throws ResponseStatusException in two cases: <ul>
+     *     <li>
+     *         If a user with the given email is not found, throws with a 404 NOT FOUND
+     *     </li>
+     *     <li>
+     *         If a user with the given email is found, but the given password is incorrect,
+     *         throws with a 401 UNAUTHORIZED
+     *     </li>
+     * </ul>
+     */
+    private UserDetails authenticateUser(String email, String password) {
         try {
             UserDetails userDetails = userDetailsService.loadUserByUsername(email);
 
@@ -33,17 +46,24 @@ public class AuthService {
                         "Incorrect password");
             }
 
-            String accessToken = jwtGenerator.generateAccessToken(userDetails);
-
-            return new AuthResponseDto(
-                    accessToken,
-                    15 * 60, // 15 mins in seconds
-                                            // I'd like to standardize this value somewhere if possible/reasonable
-                    email
-            );
+            return userDetails;
 
         } catch (UsernameNotFoundException e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
         }
+    }
+
+    public AuthResponseDto getAccessTokenFromCredentials(String email, String password) {
+
+        UserDetails userDetails = authenticateUser(email, password);
+
+        String accessToken = jwtGenerator.generateAccessToken(userDetails);
+
+        return new AuthResponseDto(
+                accessToken,
+                15 * 60, // 15 mins in seconds
+                                        // I'd like to standardize this value somewhere if possible/reasonable
+                email
+        );
     }
 }
