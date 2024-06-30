e4b6a5862b9a3a96dc3c2838cd8d458d50ae2b2d
Christian <cmmorgado53@gmail.com>
2024-06-30 17:08:38 -0400

    separated requirements

---
+++ server/src/main/java/com/degreemap/DegreeMap/courseEntities/prerequisites/GradeRequirement.java
@@ -0,0 +1,5 @@
+package com.degreemap.DegreeMap.courseEntities.prerequisites;
+
+public enum GradeRequirement {
+    S, A_PLUS, A, A_MINUS, B_PLUS, B, B_MINUS, C_PLUS, C, C_MINUS, D_PLUS, D, D_MINUS, F, NONE
+}
--- server/src/main/java/com/degreemap/DegreeMap/courseEntities/prerequisites/Prerequisite.java
+++ server/src/main/java/com/degreemap/DegreeMap/courseEntities/prerequisites/Prerequisite.java
@@ -6,10 +6,6 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "prerequisites")
 public class Prerequisite {
-    public enum GradeRequirement {
-        S, A_PLUS, A, A_MINUS, B_PLUS, B, B_MINUS, C_PLUS, C, C_MINUS, D_PLUS, D, D_MINUS, F, NONE
-    }
-    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
