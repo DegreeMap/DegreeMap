ae16430be2dc29fdd5413ab86e5b5e4355c4c1ac
Christian <cmmorgado53@gmail.com>
2024-07-10 17:12:34 -0400

    Added block controller setup

--- server/src/main/java/com/degreemap/DegreeMap/dmEntities/blocks/BlockController.java
+++ server/src/main/java/com/degreemap/DegreeMap/dmEntities/blocks/BlockController.java
@@ -1,5 +1,12 @@
 package com.degreemap.DegreeMap.dmEntities.blocks;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/blocks")
 public class BlockController {
-    
+    static class Request {
+        public String name;
+    }
 }
