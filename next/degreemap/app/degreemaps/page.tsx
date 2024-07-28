d1e8791d007e474ea24230ca78d07c5a2dd815fd
Christian <cmmorgado53@gmail.com>
2024-07-28 07:50:17 -0400

    DM Page!

---
+++ next/degreemap/app/degreemaps/page.tsx
@@ -0,0 +1,11 @@
+import NavBar from "@/components/nav/navbars";
+import React, { useEffect, useState } from "react";
+
+export default function DegreeMaps() {
+    return (
+        <>
+            <NavBar></NavBar>
+            <p>Degree Map List Page!</p>
+        </>
+    );
+}
