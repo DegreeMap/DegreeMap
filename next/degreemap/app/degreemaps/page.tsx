a4d33a81c3583d2c6accee6b716d45a4917d4aa0
Ashton Michelstein <aam1727@g.rit.edu>
2024-07-30 18:07:39 -0700

    showing shit, gotta make it show users own maps tho

--- next/degreemap/app/degreemaps/page.tsx
+++ next/degreemap/app/degreemaps/page.tsx
@@ -1,11 +1,61 @@
+"use client"
 import NavBar from "@/components/nav/navbars";
 import React, { useEffect, useState } from "react";
+import api from '@/utils/axiosInstance';
+import { useSession } from 'next-auth/react';
 
-export default function DegreeMaps() {
+
+
+const Page: React.FC = () => {
+    const { data: session } = useSession();
+    const [degreeMaps, setMaps] = useState<{ id: number, name: string }[]>([]);
+
+    const getData = async () => {
+
+        try {
+            const res = await api.get('/degreeMaps');
+
+            return res.data;
+        } catch (error: any) {
+
+            if (error.response?.status === 401) {
+                console.error('401 MOMENT');
+                console.dir(error);
+                return 'unauthorized, so you can\'t see it :(';
+            }
+
+            console.error('Error fetching dummy data: ', error);
+            return 'An error occurred';
+        }
+    }
+
+    useEffect(() => {
+        if (session) {
+            getData().then(data => {
+                setMaps(data);
+            }).catch(err => {
+                console.error(err);
+                setMaps([{ id: 0, name: "Error" }]);
+            });
+        } else {
+            setMaps([{ id: 0, name: "Must be logged in" }]);
+        }
+    }, [session]);
+
     return (
         <>
             <NavBar></NavBar>
-            <p>Degree Map List Page!</p>
+            <div>
+                <h1>Protected page</h1>
+                <h2>RESULT: </h2>
+                {degreeMaps.map((degreemap) =>
+                (
+                    <h2 key={degreemap.id}>{degreemap.name}</h2>
+                )
+                )}
+            </div>
         </>
     );
-}
+};
+
+export default Page;
