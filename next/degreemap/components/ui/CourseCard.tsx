2d4c272ef2292573057c1e3bee7eaf36924bf9dd
Christian <cmmorgado53@gmail.com>
2025-07-21 17:50:34 -0400

    Added animation to course cards when hovering over them

--- next/degreemap/components/ui/CourseCard.tsx
+++ next/degreemap/components/ui/CourseCard.tsx
@@ -10,12 +10,14 @@ interface CourseCardProps {
 export const CourseCard: React.FC<CourseCardProps> = ({ title, code, credits, onClick }) => {
 	return (
 		<div 
-            className="bg-orange-400 text-white text-xs text-center rounded px-1 py-1 cursor-pointer" 
-            onClick={onClick}>
+			className="bg-orange-400 text-white text-xs text-center rounded px-1 py-1 cursor-pointer
+					   transform transition duration-200 ease-in-out hover:scale-105"
+			onClick={onClick}
+		>
 			<div className="py-1">{title}</div>
 			<div className="bg-white text-black font-bold mt-1 rounded-b px-1">
-                <h3 className="text-[.6rem] font-semibold leading-tight pt-[3px]">{code}</h3>
-                <h3 className="text-[.6rem] font-semibold leading-none pb-[3px]">{`(${credits})`}</h3>
+				<h3 className="text-[.6rem] font-semibold leading-tight pt-[3px]">{code}</h3>
+				<h3 className="text-[.6rem] font-semibold leading-none pb-[3px]">{`(${credits})`}</h3>
 			</div>
 		</div>
 	);
